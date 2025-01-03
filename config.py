bc83049b18e0071ebe109a36fc351498dc568b72
⎯𓆩꯭ 𝐀 ꯭ʟ ꯭ᴘ ꯭ʜ꯭ ᴧ꯭🥂꯭༎꯭ 𓆪꯭ <168528642+TEAMPURVI@users.noreply.github.com>
2025-01-03 15:59:22 +0530

    ᴘᴜʀᴠɪ

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 OPENAI = getenv("OPENAI", "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")
-MONGO_URL = getenv("MONGO_URL", "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority")
-OWNER_ID = int(getenv("OWNER_ID", "7400383704"))
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://PURVIBOTS:ALPHABABY999@cluster0.v9dhx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+OWNER_ID = int(getenv("OWNER_ID", "7302887101"))
 SUPPORT_GRP = "PURVI_UPDATES"
 UPDATE_CHNL = "PURVI_SUPPORT"
 OWNER_USERNAME = "ll_ALPHA_BABY_lll"
@@ -26,4 +26,11 @@ IMG = [
     "https://files.catbox.moe/99wj12.jpg",
     "https://files.catbox.moe/ezpnd2.jpg",
     "https://files.catbox.moe/e7q55f.jpg",
+    "https://files.catbox.moe/qyfsi7.jpg",
+    "https://files.catbox.moe/kbke7s.jpg",
+    "https://files.catbox.moe/7icvpu.jpg",
+    "https://files.catbox.moe/4hd77z.jpg",
+    "https://files.catbox.moe/yn7wje.jpg",
+    "https://files.catbox.moe/kifsir.jpg",
+    "https://files.catbox.moe/zi21kc.jpg",
 ]
